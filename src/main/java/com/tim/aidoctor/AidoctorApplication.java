6c42e611195651c71c7d74419e4f7fd1715bacd2
tim <tim-zhang@live.com>
2025-05-27 21:47:20 -0600

    basic things to get started

--- src/main/java/com/tim/aidoctor/AidoctorApplication.java
+++ src/main/java/com/tim/aidoctor/AidoctorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AidoctorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AidoctorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AidoctorApplication.class, args);
+    }
 
 }
--- src/test/java/com/tim/aidoctor/AidoctorApplicationTests.java
+++ src/test/java/com/tim/aidoctor/AidoctorApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AidoctorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
