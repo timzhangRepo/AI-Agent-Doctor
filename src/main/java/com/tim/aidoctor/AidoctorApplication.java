764f3c680aa3cda15cbac2b422f23aa1328332c4
tim <tim-zhang@live.com>
2025-05-27 21:35:47 -0600

    Initial commit

---
+++ src/main/java/com/tim/aidoctor/AidoctorApplication.java
@@ -0,0 +1,13 @@
+package com.tim.aidoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tim/aidoctor/AidoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.aidoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
